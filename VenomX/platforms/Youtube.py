2d291e88d2477d7ceabac6348292279a895eec38
Anurag Dev <129645919+noobpiratexd@users.noreply.github.com>
2025-01-12 19:31:32 +0530

    Update Youtube.py

--- VenomX/platforms/Youtube.py
+++ VenomX/platforms/Youtube.py
@@ -8,7 +8,7 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
-from pybal import cobalt
+from pybalt import cobalt
 
 from VenomX.utils.database import is_on_off
 from VenomX.utils.formatters import time_to_seconds
